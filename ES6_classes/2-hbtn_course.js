51aa83ec2f5015548c51d0dcc4a17e8c775c20dd
ericpo1sh <ericpo1sh@gmail.com>
2024-01-07 15:05:05 -0600

    task 2 done?

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    students.forEach((student) => {
+      if (typeof student !== 'string') {
+        throw TypeError('Students must be an array of strings');
+      }
+    });
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    students.forEach((student) => {
+      if (typeof student !== 'string') {
+        throw TypeError('Students must b ean array of strings');
+      }});
+    this._students = students
+    }
+
+  get students() {
+    return this._students;
+  }
+}

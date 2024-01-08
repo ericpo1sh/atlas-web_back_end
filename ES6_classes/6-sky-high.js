12b4e12d9248099c664c37999f113259c676bf9e
ericpo1sh <ericpo1sh@gmail.com>
2024-01-08 12:11:28 -0600

    removed setters

---
+++ ES6_classes/6-sky-high.js
@@ -0,0 +1,26 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('Sqft must be a number');
+    }
+    if (typeof floors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+    this._sqft = sqft;
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return self._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return "Evacuate slowly the NUMBER_OF_FLOORS floors";
+  }
+}

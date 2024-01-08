8699aa62c159e0f1345e6b42f1ce0548297e2a71
ericpo1sh <ericpo1sh@gmail.com>
2024-01-07 22:51:13 -0600

    task 4 done first try

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,51 @@
+import Currency from './3-currency.js';
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    if (typeof currency !== Currency) {
+      throw TypeError('Currency must be of type Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    } else {
+      this._amount = amount;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (typeof currency !== Currency) {
+      throw TypeError('Currency must be of type Currency');
+    } else {
+      this._currency = currency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw TypeError('ConversionRate must be a number');
+    }
+    return (amount * conversionRate);
+  }
+}

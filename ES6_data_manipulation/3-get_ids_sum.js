1914648635362fb240a8ac3329c6ee0a0e3a82e8
ericpo1sh <ericpo1sh@gmail.com>
2024-01-08 14:14:46 -0600

    Create a function getStudentIdsSum that returns the sum of all the student ids.

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,7 @@ export default function getStudentIdsSum(students) {
   if (Object.getPrototypeOf(students) !== Array.prototype) {
     return [];
   }
-  const listOfIds = students.map((student) => student.id );
+  const listOfIds = students.map((student) => student.id);
   const initialValue = 0;
   const sumWithInitial = listOfIds.reduce(
     (accumulator, currentValue) => accumulator + currentValue,

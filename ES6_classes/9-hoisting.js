2759becf089db20a35623efe8a00974227d33405
ericpo1sh <ericpo1sh@gmail.com>
2024-01-08 12:46:17 -0600

    lint fix and typo fixes

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -3,11 +3,11 @@ export class HolbertonClass {
     this._year = year;
     this._location = location;
   }
-  
+
   get year() {
     return this._year;
   }
-  
+
   get location() {
     return this._location;
   }
@@ -19,15 +19,15 @@ export class StudentHolberton {
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
   }
-  
+
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
-  
+
   get holbertonClass() {
     return this.holbertonClass;
   }
-  
+
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
@@ -41,4 +41,5 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;

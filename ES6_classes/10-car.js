066311345950b4675707f9edd921c99193b18c45
ericpo1sh <ericpo1sh@gmail.com>
2024-01-08 13:16:07 -0600

    task 10 done

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,25 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      throw TypeError('Brand must be a string');
+    }
+    if (typeof motor !== 'string') {
+      throw TypeError('Motor must be a string');
+    }
+    if (typeof color !== 'string') {
+      throw TypeError('Color must be a string');
+    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color);
+  }
+}

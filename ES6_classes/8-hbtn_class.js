1061fd9a4f8916ab1138fbba8483277b17971db1
ericpo1sh <ericpo1sh@gmail.com>
2024-01-08 12:34:49 -0600

    task 8 done

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,21 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw TypeError('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+        return this._size;
+    } else if (hint === 'string') {
+        return this._location;
+    }
+    return this;
+  }
+}

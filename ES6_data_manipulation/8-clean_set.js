077e27e82e11cab7bcc070b6deeb0b42a9745e00
ericpo1sh <ericpo1sh@gmail.com>
2024-01-09 11:06:05 -0600

    turnary return added

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,10 @@
 export default function cleanSet(set, startString) {
-  const finalString = "";
+  let finalString = "";
   for (const word of set) {
     const stringWord = String(word);
     if (stringWord.startsWith(startString)) {
       const slicedWord = stringWord.slice(startString.length);
-      finalString += slicedWord;
-      if (finalString.length % 2 !== 0 && finalString.length > 1) {
-        finalString += "-";
-      }
+      finalString += (finalString.length !== 0 && slicedWord.length !== 0) ? `-${slicedWord}` : slicedWord;
     }
   }
   return finalString;

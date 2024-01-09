59901b59a2f848db8850f723aad93f6dd8345c2e
ericpo1sh <ericpo1sh@gmail.com>
2024-01-09 10:55:05 -0600

    Create a function named cleanSet that returns a string of all the set values that start with a specific string (startString).

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  const finalString = "";
+  for (const word of set) {
+    if (word.has(startString)) {
+      word.slice(startString.length) += finalString;
+      if (finalString.length % 2 !== 0) {
+        finalString += "-";
+      }
+    }
+  }
+}

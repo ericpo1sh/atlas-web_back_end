507a4607ab8895f84755bdb2c19f099a445fb29b
ericpo1sh <ericpo1sh@gmail.com>
2024-01-09 13:06:58 -0600

    create a function that returns object and log when promise resolves, error and log when rejects

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (resolve) {
-      resolve({ status: 200, body: 'Success' })
-      console.log('Got a response from the API')
-    }
-    if (reject) {
-      reject(Error);
-    }
+  return promise.then(() => {
+    console.log('Got a response from the API');
+    return ({ status: 200, body: 'success' });
+  }).catch(() => {
+    console.log('Got a response from the API');
+    return new Error();
   });
 }

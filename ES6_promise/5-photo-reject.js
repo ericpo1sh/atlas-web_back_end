87722d3f2d600d15ff2255ad55640ad4394d8676
ericpo1sh <ericpo1sh@gmail.com>
2024-01-09 13:46:50 -0600

    added missing error object

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
   return new Promise((reject) => {
-    reject(`${fileName} cannot be processed`);
+    reject(Error(`${fileName} cannot be processed`));
   });
 }

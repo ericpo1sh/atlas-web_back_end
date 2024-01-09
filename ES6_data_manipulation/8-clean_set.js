330c100d3dcc1f93e10e5abdf5a83907e52951bb
ericpo1sh <ericpo1sh@gmail.com>
2024-01-09 10:56:37 -0600

    fix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -3,9 +3,10 @@ export default function cleanSet(set, startString) {
   for (const word of set) {
     if (word.has(startString)) {
       word.slice(startString.length) += finalString;
-      if (finalString.length % 2 !== 0) {
+      if (finalString.length % 2 !== 0 && finalString.length > 1) {
         finalString += "-";
       }
     }
   }
+  return finalString;
 }

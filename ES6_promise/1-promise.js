5ecc6224138b2f54ada99610c1630197976895d1
ericpo1sh <ericpo1sh@gmail.com>
2024-01-09 12:22:38 -0600

    lint fix

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -2,9 +2,8 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
       resolve({ status: 200, body: 'Success' });
-    }
-    else {
-      reject(Error("The fake API is not working currently"));
+    } else {
+      reject(Error('The fake API is not working currently'));
     }
   });
 }

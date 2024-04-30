db0647fed8f74db9fbd982082e2f7c0b880285dc
ericpo1sh <ericpo1sh@gmail.com>
2024-04-30 17:19:17 -0400

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,5 +2,8 @@
 # function that casts A and B into floats and returns the sum as a float.
 
 
-def add(a: float, b: float) -> float:  # casting
-    return a + b  # return
+def add(a: float, b: float) -> float:
+    '''a : first float number
+    b : second float number
+    return: sum of two floats'''
+    return a + b

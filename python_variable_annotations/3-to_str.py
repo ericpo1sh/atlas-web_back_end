dc49f357efc3104e7beef4489c343d67bf85a3f3
ericpo1sh <ericpo1sh@gmail.com>
2024-04-30 17:34:20 -0400

    3. Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' Module Documentation '''
+
+
+def to_str(n: float) -> str:
+    ''' float n as argument and returns
+    the string representation of the float. '''
+    return str(n)

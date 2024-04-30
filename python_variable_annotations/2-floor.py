95a0778967bdffc86126424315bbaeb5cb63156b
ericpo1sh <ericpo1sh@gmail.com>
2024-04-30 17:30:43 -0400

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' function floor which takes a float n and returns the floor of the float '''
+from math import floor as math_floor
+
+
+def floor(n: float) -> int:
+    ''' casting each element '''
+    return math_floor(n)

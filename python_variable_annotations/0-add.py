5fbf8e19d185d9e0fa35bf38ce4377f5c847aa68
ericpo1sh <ericpo1sh@gmail.com>
2024-04-30 17:22:30 -0400

    comments fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# function that casts A and B into floats and returns the sum as a float.
+''' function that casts A and B into floats and returns the sum as a float. '''
 
 
 def add(a: float, b: float) -> float:

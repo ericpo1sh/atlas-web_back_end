8758c4dcfea22f83d08b8994ba4110548be151ca
ericpo1sh <ericpo1sh@gmail.com>
2024-04-30 17:25:32 -0400

    Basic annotations - concat

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+''' function that concatinates two strings into one '''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' typecasting everything to be a string '''
+    return str1 + str2

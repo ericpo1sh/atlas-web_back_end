e265cd0a5098594b50da2b98404ba5894c1f2527
ericpo1sh <ericpo1sh@gmail.com>
2024-05-02 11:17:57 -0500

    2. Run time for four parallel comprehensions

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,12 +8,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     ''' measure the total run time '''
     start = time.time()
-    await asyncio.gather(
-      async_comprehension(),
-      async_comprehension(),
-      async_comprehension(),
-      async_comprehension()
-    )
+    tasks = [asyncio.create_task(async_comprehension()) for _ in range(4)]
+    await asyncio.gather(*tasks)
     end = time.time()
     total = end - start
     return total

ba3893be3ce18b74122de655f5fc57b8ca255f8f
ericpo1sh <ericpo1sh@gmail.com>
2024-05-06 11:52:09 -0500

    0. Basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -4,7 +4,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCaching defines:
+    """ BasicCache defines:
     constants of your caching system
     where your data are stored (in a dictionary) """
     def __init__(self):

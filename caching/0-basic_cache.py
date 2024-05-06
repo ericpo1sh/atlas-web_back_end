ad21473aee15c3e6bfceb08ba3c7389ae2d1d95a
ericpo1sh <ericpo1sh@gmail.com>
2024-05-06 11:53:37 -0500

    0. Basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -12,11 +12,13 @@ class BasicCache(BaseCaching):
         self.cache_data = {}
 
     def put(self, key, item):
+        ''' put '''
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        ''' get '''
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]

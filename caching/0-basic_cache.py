9fa24223b74c8c48ae9c9f5cb4010f82173678d6
ericpo1sh <ericpo1sh@gmail.com>
2024-05-06 13:34:54 -0500

    1. FIFO caching

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -10,8 +10,8 @@ class BasicCache(BaseCaching):
         ''' put: assign to the dictionary self.cache_data the item value. '''
         if key is None or item is None:
             return
-        self.cache_data[key] = item
 
+        self.cache_data[key] = item
     def get(self, key):
         ''' get : return the value in self.cache_data linked to key '''
         if key is None or key not in self.cache_data:
---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" BasicCache module """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    ''' FIFOCaching Dictionary class '''
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        ''' assign to the dictionary self.cache_data the item value for the key key. '''
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = next(iter(self.cache_data))
+            del self.cache_data[discard]
+            print(f"DISCARD: {discard}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        ''' Must return the value in self.cache_data linked to key. '''
+        if key is None or key not in self.cache_data:
+            return
+        return self.cache_data[key]

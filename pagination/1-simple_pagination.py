617b2b35e4dbf338cdf79e2e77ad2059ecc4d26b
ericpo1sh <ericpo1sh@gmail.com>
2024-05-07 11:23:55 -0500

    1. Simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Simple Pagination file
+"""
+import csv
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        return dataset[start_index:end_index]
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """  return a tuple of size two,
+    containing a start index and an end index based on page number """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index

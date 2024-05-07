aafbb26884f482f66c7b9fcd704c321946f73670
ericpo1sh <ericpo1sh@gmail.com>
2024-05-07 10:23:07 -0500

    0. Simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Helper Function Module """
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """  return a tuple of size two,
+    containing a start index and an end index based on page number """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index

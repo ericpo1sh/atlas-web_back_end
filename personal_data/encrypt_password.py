2ec7848b2dad504c20d7be2c27fe78cf3cebd919
ericpo1sh <ericpo1sh@gmail.com>
2024-05-10 14:01:33 -0500

    Check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,11 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     ''' function that uses bcrypt and returns a hashed password '''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' Check valid password  '''
+    if bcrypt.checkpw(hashed_password, password):
+        return True
+    else:
+        return False

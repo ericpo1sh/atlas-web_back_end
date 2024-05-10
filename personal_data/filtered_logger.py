761199e59451fc5bda598221ec96ddda08d15920
ericpo1sh <ericpo1sh@gmail.com>
2024-05-10 13:33:46 -0500

    saving changed for filtered logger, burnt out tring to figure this out rn

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Filtered Logger file
 """
 import re
+import logging
 from typing import List
 
 
@@ -15,3 +16,23 @@ def filter_datum(
         lambda match: match.group(1) + '=' + redaction,
         message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        ''' modified class to accept fields as an arguement '''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        ''' implemented method to filter incoming log records using func '''
+        filter_datum(self.fields, self.REDACTION, )
+        NotImplementedError
+

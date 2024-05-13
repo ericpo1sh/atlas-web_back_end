0c404858c9984e4b5b517c75b6dce210a5b7048f
ericpo1sh <ericpo1sh@gmail.com>
2024-05-13 14:27:40 -0500

    testing if update passes the checker

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,12 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         ''' returns None - request will be the Flask request object '''
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        else:
+            return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         ''' returns None - request will be the Flask request object '''

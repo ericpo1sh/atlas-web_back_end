58c4cce46313e4bb0bcde162d2d2dfe006a28cc2
ericpo1sh <ericpo1sh@gmail.com>
2024-05-13 13:19:31 -0500

    checking if auth passes checker

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+''' Auth Class Module '''
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    ''' create a class to manage the API authentication. '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        ''' that returns False - path and excluded_paths '''
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        ''' returns None - request will be the Flask request object '''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''' returns None - request will be the Flask request object '''
+        return None

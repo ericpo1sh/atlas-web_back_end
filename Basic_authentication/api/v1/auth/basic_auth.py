3433ed9b1f031c8f40a5803ba217e68d2c6ef2a1
ericpo1sh <ericpo1sh@gmail.com>
2024-05-14 11:07:06 -0500

    7. Basic - Base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 ''' Basic Auth Class Module '''
-from api.v1.auth.auth import Auth
+from auth import Auth
 
 
 class BasicAuth(Auth):
     ''' class BasicAuth that inherits from Auth. '''
-    pass
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        ''' returns the Base64 part of the Authorization header '''
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]

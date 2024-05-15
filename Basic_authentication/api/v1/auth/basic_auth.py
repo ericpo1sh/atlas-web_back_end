4ec564008a6441e6037b404a84a81c2d53038883
ericpo1sh <ericpo1sh@gmail.com>
2024-05-15 09:58:13 -0500

    fix

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -77,11 +77,8 @@ class BasicAuth(Auth):
             return None
 
         header = request.headers.get('Authorization')
-        base64_header = self.extract_base64_authorization_header
-        (header)
-        decoded_header = self.decode_base64_authorization_header
-        (base64_header)
-        user_email, user_pwd = self.extract_user_credentials
-        (decoded_header)
+        base64_header = self.extract_base64_authorization_header(header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        user_email, user_pwd = self.extract_user_credentials(decoded_header)
 
         return self.user_object_from_credentials(user_email, user_pwd)

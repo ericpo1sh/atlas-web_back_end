9f3340b4665ae2063aef7f25e34ec903ac88f261
ericpo1sh <ericpo1sh@gmail.com>
2024-05-16 11:10:32 -0500

    1. Empty session

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' Session Auth Class Module '''
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    ''' class SessionAuth that inherits from Auth. '''
+    pass

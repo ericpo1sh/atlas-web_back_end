2e13c9441778f820e14ecaef6c911f6e89674f84
ericpo1sh <ericpo1sh@gmail.com>
2024-05-16 18:38:52 -0500

    5. Before request, issue resolved hopefully

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -36,14 +36,14 @@ def before_request():
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
     if not auth.authorization_header(
      request) and not auth.session_cookie(request):
+        print('1')
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
+    print('2')
 
 
 @app.errorhandler(401)

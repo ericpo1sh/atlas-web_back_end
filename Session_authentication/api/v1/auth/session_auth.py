38b76f8f673488d49e72343036d0bbb4d5b3343d
ericpo1sh <ericpo1sh@gmail.com>
2024-05-16 11:30:44 -0500

    3. User ID for Session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -9,10 +9,16 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        ''' Creates a session for the UserID '''
+        ''' Creates a session for the User ID '''
         if user_id is None or not isinstance(user_id, str):
             return None
         else:
             session_id = uuid.uuid4()
             self.user_id_by_session_id[str(session_id)] = user_id
             return str(session_id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        ''' returns a User ID based on a Session ID: '''
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)

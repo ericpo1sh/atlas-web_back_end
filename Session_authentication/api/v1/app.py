4bcefee95abf03c94862fce9ee1c0855a669fedf
ericpo1sh <ericpo1sh@gmail.com>
2024-05-16 16:51:58 -0500

    t

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -33,10 +33,13 @@ def before_request():
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
+        if auth.session_cookie(request):
+            abort(401)
         abort(401)
     if auth.current_user(request) is None:
         abort(403)

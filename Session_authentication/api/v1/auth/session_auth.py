a92787bd79178f3c9bc77c48b6d5d814e1458c29
ericpo1sh <ericpo1sh@gmail.com>
2024-05-16 11:25:44 -0500

    2. Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 ''' Session Auth Class Module '''
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     ''' class SessionAuth that inherits from Auth. '''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        ''' Creates a session for the UserID '''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id = uuid.uuid4()
+            self.user_id_by_session_id[str(session_id)] = user_id
+            return str(session_id)

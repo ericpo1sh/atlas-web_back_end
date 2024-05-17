530f330e60091d9a82658d2e782d780806839d1c
ericpo1sh <ericpo1sh@gmail.com>
2024-05-17 13:35:21 -0500

    routing fixed, error codes added

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -9,23 +9,29 @@ from api.v1.views import app_views
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    ''' New view for Session Authentication  '''
+    ''' New view for Session Authentication login '''
     email = request.form.get('email')
     if not email:
-        return jsonify({"error:" "email missing"}), 400
+        return jsonify({"error": "email missing"}), 400
+
     password = request.form.get('password')
     if not password:
-        return jsonify({"error:" "password missing"}), 400
+        return jsonify({"error": "password missing"}), 400
+
     search_user = User.search({"email": email})
     if not search_user:
-        return jsonify({"error:" "no user found with this email"})
+        return jsonify({"error:" "no user found with this email"}), 404
+
     user = search_user[0]
     if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"})
+        return jsonify({"error": "wrong password"}), 401
+
     from api.v1.app import auth
+
     session_id = auth.create_session(user.id)
-    user_response = make_response(user)
-    session = os.getenv('SESSION_NAME')
-    user_response.setcookie(session, session_id)
     to_json_user = user.to_json()
-    return to_json_user
+    response = make_response(to_json_user)
+    session = os.getenv('SESSION_NAME')
+    response.set_cookie(session, session_id)
+
+    return response

16da86d7fa8dd50d210be411a1c44849d3946471
ericpo1sh <ericpo1sh@gmail.com>
2024-05-20 13:47:02 -0500

    5. Register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 """ Auth Model """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
-def _hash_password(password: str) -> bytes:
-    '''  method that takes in a password
-    string arguments and returns bytes. '''
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        ''' take mandatory email and password
+        string arguments and return a User object. '''
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_pw = self._hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=hashed_pw)
+            return user
+
+    def _hash_password(self, password: str) -> bytes:
+        '''  method that takes in a password
+        string arguments and returns bytes. '''
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())

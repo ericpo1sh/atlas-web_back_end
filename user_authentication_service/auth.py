00a8f0bf8de4dbc38b7613d46cf1ec318a0e1876
ericpo1sh <ericpo1sh@gmail.com>
2024-05-21 14:27:40 -0500

    16. Generate reset password token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -102,9 +102,9 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         ''' Generate reset password token  '''
         try:
-            self._db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
         except NoResultFound:
             raise ValueError
-        new_token = self._generate_uuid()
-        self._db.update_user(email, reset_token=new_token)
+        new_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=new_token)
         return new_token

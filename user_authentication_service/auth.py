50ae6d272b1fc3e9070ae9ab022c5b3e3d310028
ericpo1sh <ericpo1sh@gmail.com>
2024-05-21 14:13:35 -0500

    fix?

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -101,15 +101,16 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         ''' Generate reset password token  '''
-        if email is None:
-            raise ValueError
-        try:
-            user = self._db.find_user_by(email=email)
-            if user:
+        if email is not None:
+            try:
+                user = self._db.find_user_by(email=email)
+                if not user:
+                    raise ValueError
                 reset_token = uuid.uuid4()
-                self._db.update_user(user_id=user.id, reset_token=reset_token)
+                self._db.update_user(user_id=user.id,
+                                     reset_token=reset_token)
                 return reset_token
-        except NoResultFound:
-            pass
-        except InvalidRequestError:
-            pass
+            except NoResultFound:
+                pass
+            except InvalidRequestError:
+                pass

50c57709112f6475541be797716e32c74e1bf0aa
ericpo1sh <ericpo1sh@gmail.com>
2024-05-21 13:17:24 -0500

    changed conditional

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -48,7 +48,7 @@ def logout():
     If the user exists destroy the session '''
     current_session = request.cookies.get('session_id', None)
     current_user = AUTH.get_user_from_session_id(current_session)
-    if current_user is None and current_session is None:
+    if current_user is None or current_session is None:
         abort(403)
     AUTH.destroy_session(current_user.id)
     return redirect('/')

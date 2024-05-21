c8a15d52427513c72800d4f38d4058025d5f800d
ericpo1sh <ericpo1sh@gmail.com>
2024-05-21 14:26:10 -0500

    fi?

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -102,7 +102,7 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         ''' Generate reset password token  '''
         try:
-            user = self._db.find_user_by(email=email)
+            self._db.find_user_by(email=email)
         except NoResultFound:
             raise ValueError
         new_token = self._generate_uuid()

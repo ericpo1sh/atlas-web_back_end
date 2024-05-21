b1ee1ee26b6a554d063e9e1cb89d2c29475d2b60
ericpo1sh <ericpo1sh@gmail.com>
2024-05-21 10:16:44 -0500

    7. Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -13,5 +13,18 @@ def firstmessage():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'])
+def users():
+    ''' checks if the user exists and returns based on existance '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email=email, password=password)
+        return jsonify({"email": "<registered email>",
+                        "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

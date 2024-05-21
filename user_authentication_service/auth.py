728fe74710adbb38760396ff03a7edb9acd42cdd
ericpo1sh <ericpo1sh@gmail.com>
2024-05-21 10:53:32 -0500

    def _generate_uuid() -> str:

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -14,6 +14,11 @@ def _hash_password(self, password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+        ''' function that generates a new uuid '''
+        return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """

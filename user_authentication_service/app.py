a1f2fc378eee8e763ea1aa2b8f1fc64a955ef802
ericpo1sh <ericpo1sh@gmail.com>
2024-05-21 14:58:31 -0500

    FINALLY

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -69,7 +69,7 @@ def get_reset_password_token():
     try:
         email = request.form.get('email')
         token = AUTH.get_reset_password_token(email)
-        return jsonify({{"email": email, "reset_token": token}}), 200
+        return jsonify({"email": email, "reset_token": token}), 200
     except ValueError:
         abort(403)
 

56e45f3eff778f8bf5d1df22ebc07fff509208b0
ericpo1sh <ericpo1sh@gmail.com>
2024-05-21 15:25:16 -0500

    it is finished

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -117,4 +117,4 @@ class Auth:
         except NoResultFound:
             raise ValueError
         new_pass = _hash_password(password)
-        self._db.update_user(user.id, new_password=new_pass, reset_token=None)
+        self._db.update_user(user.id, password=new_pass, reset_token=None)

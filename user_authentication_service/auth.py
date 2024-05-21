bb720f9caaf833d1b9ef0d64ff6a71fe9ef6575b
ericpo1sh <ericpo1sh@gmail.com>
2024-05-21 15:11:18 -0500

    18. Update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -108,3 +108,15 @@ class Auth:
         new_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=new_token)
         return new_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        ''' Uses the reset_token to find the corresponding user
+        and updates their hashed password '''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        new_password = _hash_password(password)
+        self._db.update_user(user.id, new_password=new_password,
+                             reset_token=None)
+        return None

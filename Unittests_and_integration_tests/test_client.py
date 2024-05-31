080da0d2ec1362d0ee8158b7e0492f30bddb9fc7
ericpo1sh <ericpo1sh@gmail.com>
2024-05-31 14:04:15 -0500

    7. Parameterize

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -52,3 +52,14 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(repos, ["repo1", "repo2", "repo3"])
             mock_public_repos_url.assert_called_once()
             mock_get_json.assert_called_once_with(mock_repos_url)
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({"license": None}, "my_license", False),
+        ({}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        ''' testing licenses '''
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)

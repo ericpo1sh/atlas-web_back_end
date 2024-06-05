669769b030c3574471c72857bd816aa63d527eee
ericpo1sh <ericpo1sh@gmail.com>
2024-06-05 11:39:09 -0500

    0. Writing strings to Redis

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    ''' Cache Class '''
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        ''' method to store things '''
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key

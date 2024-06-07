6c38bf194fe3bab626ebbdb159811497fe56dbd3
ericpo1sh <ericpo1sh@gmail.com>
2024-06-07 17:31:27 -0500

    fix

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Write a SQL script that creates a table users
-CREATE 'users' TABLE IF NOT EXISTS 'users' (
+CREATE TABLE users IF NOT EXISTS (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)

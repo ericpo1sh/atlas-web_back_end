650faec2463d3b4eb466ea27b6130f5fa6e1a8d7
ericpo1sh <ericpo1sh@gmail.com>
2024-06-10 11:04:15 -0500

    SQL Script that ranks country origins by the num of non-unique fans

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,1 +1,10 @@
 -- SQL Script that ranks country origins by the num of non-unique fans
+SELECT
+  origin AS origin,
+  SUM(fans) AS nb_fans
+FROM
+  metal_bands
+GROUP BY
+  origin
+ORDER BY
+  nb_fans DESC;

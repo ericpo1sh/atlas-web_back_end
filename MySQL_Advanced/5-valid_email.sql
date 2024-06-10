6ae96b06e97add54942f179dd1eff4bafb90e30e
ericpo1sh <ericpo1sh@gmail.com>
2024-06-10 12:30:02 -0500

    added delimiter

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,6 @@
 -- SQL script that creates a trigger that resets the attribute
 -- valid_email only when the email has been changed.
+DELIMITER //
 CREATE TRIGGER email_validation_reset
 BEFORE UPDATE ON users
 FOR EACH ROW

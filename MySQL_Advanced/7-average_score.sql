b27abbb6f9a8c1bb49fa8ebe5b5fba8fdc3f6605
ericpo1sh <ericpo1sh@gmail.com>
2024-06-10 14:06:02 -0500

    fix

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -7,7 +7,7 @@ BEGIN
   SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
   UPDATE users
   SET average_score = avg_score
-  WHERE id = user_id
+  WHERE id = user_id;
 END //
 
 DELIMITER ;

93783ef4921cfde88b72fc60918bf80e7574faee
ericpo1sh <ericpo1sh@gmail.com>
2024-06-11 13:23:46 -0500

    changed type to list

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     ''' returns the list of school having a specific topics '''
-    return mongo_collection.find(topic)
+    return list(mongo_collection.find({"topics": topic}))

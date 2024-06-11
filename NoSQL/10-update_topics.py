9b45774fcf960d81c2e2058de2f0748fe8695296
ericpo1sh <ericpo1sh@gmail.com>
2024-06-11 13:17:02 -0500

    fixed function to properly update multiple

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,5 +6,4 @@ def update_topics(mongo_collection, name, topics):
     ''' changes all topics of a school document based on the name '''
     topics_to_update = {"$set": {"topics": topics}}
     key_to_update = {"name": name}
-    update = mongo_collection.update_one(key_to_update, topics_to_update)
-    return update
+    mongo_collection.update_many(key_to_update, topics_to_update)

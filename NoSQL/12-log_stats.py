550ff6039b21cfbfa723db8d25aebeb688bac5ac
ericpo1sh <ericpo1sh@gmail.com>
2024-06-11 13:55:35 -0500

    12. Log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" 12. Log stats """
+from pymongo import MongoClient
+
+
+def log_stats():
+    ''' script that provides some stats about Nginx logs stored in MongoDB '''
+    client = MongoClient()
+    db = client.logs
+    logs = db.nginx
+    count_logs = logs.count_documents({})
+    print(f'{count_logs} logs')
+    print('Methods:')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count_method = logs.count_documents({"method": method})
+        print(f'\tmethod {method}: {count_method}')
+    status = logs.count_documents({'method': 'GET', 'path': '/status'})
+    print(f'{status} status check')

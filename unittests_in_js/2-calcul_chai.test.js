01461392db6a88ac802727f5412c857d5a7ada82
ericpo1sh <ericpo1sh@gmail.com>
2024-06-20 13:49:16 -0500

    fix?

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -14,16 +14,6 @@ describe("calculateNumber", function() {
   });
 
   describe('SUBTRACT', () => {
-    it('should return -4 when inputs are 1.4 and 4.5', () => {
-      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    });
-
-    it('should return 0 when inputs are 1.4 and 1.4', () => {
-      expect(calculateNumber('SUBTRACT', 1.4, 1.4)).to.equal(0);
-    });
-  });
-
-  describe('SUBTRACT', () => {
     it("works properly with SUBTRACT, normal params", function() {
       expect(calculateNumber('SUBTRACT', 3, 2)).to.equal(1);
     });

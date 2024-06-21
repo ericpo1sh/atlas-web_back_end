30daa60af9ce87ee9ee1af24523ddf83fb2d280d
ericpo1sh <ericpo1sh@gmail.com>
2024-06-21 13:16:58 -0500

    fix?

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,14 +1,12 @@
-const sendPaymentRequestToApi = require("./3-payment");
-const Utils = require("./utils");
 const sinon = require('sinon');
-const chai = require('chai');
-const expect = chai.expect;
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToAPI', function() {
-  let spy = sinon.spy(Utils, 'sendPaymentRequestToApi');
-  it('makes sure the math is the same used as the utils method', function () {
-    sendPaymentRequestToApi(99, 30);
-    expect(spy.calledOnce).to.be.true;
-    expect(spy.calledWith('SUM', 99, 30)).to.be.true;
-  });
+describe('sendPaymentRequestToApi', function () {
+    var spy = sinon.spy(Utils, 'calculateNumber');;
+    it('makes sure the math is the same used as the utils method', function () {
+        sendPaymentRequestToApi(20, 5);
+        expect(spy.calledOnceWithExactly('SUM', 20, 5)).to.be.true;
+    });
 });

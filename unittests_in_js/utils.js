32d814ea99efd181346cdc06106b73fd19d61530
ericpo1sh <ericpo1sh@gmail.com>
2024-06-21 13:25:37 -0500

    fixed, 3. Spies

--- unittests_in_js/utils.js
+++ unittests_in_js/utils.js
@@ -7,7 +7,7 @@ const Utils = {
       return Math.round(a) - Math.round(b);
     }
     if (type === 'DIVIDE') {
-      if (b === 0) {
+      if (Math.round(b) === 0) {
         return 'Error';
       }
       return Math.round(a) / Math.round(b);

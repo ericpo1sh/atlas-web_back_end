0006324baa9bad42cdcf17e851036893e95a0368
ericpo1sh <ericpo1sh@gmail.com>
2024-06-24 10:21:01 -0500

    9. Regex integration testing fixed

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,6 +1,24 @@
 const request = require('request');
 const expect = require('chai').expect;
 
+describe('Test suite for the index page', function() {
+  it('Checks if the status code is the same', function(done) {
+    request('http://localhost:7865/', function(err, response) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    })
+  })
+  it('Checks if the body returns the correct result', function(done) {
+    request('http://localhost:7865/', function(err, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    })
+  })
+  it('checks whatever "other" is...', function(done) {
+    done();
+  })
+})
+
 describe('Test suite for the cart page', function() {
   it('Checks if the status code is the same', function(done) {
     request('http://localhost:7865/cart/100', function(err, response) {
@@ -14,7 +32,10 @@ describe('Test suite for the cart page', function() {
       done();
     })
   })
-  it('checks whatever "etc" is...', function(done) {
-    done();
+  it('checks if the body response is correct', function(done) {
+    request('http://localhost:7865/cart/14', function(err, response, body) {
+      expect(body).to.equal('Payment methods for cart 14')
+      done();
+    })
   })
 })

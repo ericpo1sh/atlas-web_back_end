6605e97ddae90f9493f0daf24be1ccac79af3e68
ericpo1sh <ericpo1sh@gmail.com>
2024-06-24 11:50:00 -0500

    fixed file

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -1,14 +1,13 @@
 // testing for payment_token function
 const sinon = require('sinon');
 const { expect } = require('chai');
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./6-payment_token');
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function() {
   it('should return a resolved promise object if true', function(done) {
     getPaymentTokenFromAPI(true).then(response => {
       expect(response).to.have.property('data', 'Successful response from the API');
       done();
-    }).catch(err => done(err));;
+    }).catch(err => done(err));
   })
 })

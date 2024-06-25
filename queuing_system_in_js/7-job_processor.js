3c090ffc2ab14d8d6cd9e99e69e2a79586d4c411
ericpo1sh <ericpo1sh@gmail.com>
2024-06-25 12:50:48 -0500

    fixed

--- queuing_system_in_js/7-job_processor.js
+++ queuing_system_in_js/7-job_processor.js
@@ -10,6 +10,7 @@ function sendNotification(phoneNumber, message, job, done) {
   }
   job.progress(50, 100);
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
 }
 
 queue.process('push_notification_code_2', 2, (job, done) => {
---
+++ queuing_system_in_js/8-job.test.js
@@ -0,0 +1,9 @@
+import createPushNotificationsJobs from './8-job.js';
+const expect = require('chai').expect;
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+describe('createPushNotificationsJobs', function() {
+  
+})

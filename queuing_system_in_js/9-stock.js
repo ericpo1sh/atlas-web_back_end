679f8c1e33f440ed4f9c8c6b89e9ef4355ac5e61
ericpo1sh <ericpo1sh@gmail.com>
2024-06-26 12:29:35 -0500

    saving changes

--- queuing_system_in_js/9-stock.js
+++ queuing_system_in_js/9-stock.js
@@ -1,37 +1,31 @@
 const listProducts = [
   {
-    Id: 1,
+    id: 1,
     name: 'Suitcase 250',
     price: 50,
     stock: 4
   },
   {
-    Id: 2,
+    id: 2,
     name: 'Suitcase 450',
     price: 100,
     stock: 10
   },
   {
-    Id: 3,
+    id: 3,
     name: 'Suitcase 650',
     price: 350,
     stock: 2
   },
   {
-    Id: 4,
+    id: 4,
     name: 'Suitcase 1050',
     price: 550,
     stock: 5
   }
 ]
 function getItemById(id) {
-  let target_id;
-  listProducts.forEach((product) => {
-    if (product.Id === id) {
-      target_id = product
-    }
-  })
-  return target_id;
+  return listProducts.find(item => item.id == id)
 }
 
 const express = require('express');
@@ -39,12 +33,29 @@ const app = express();
 const port = 1245;
 
 app.get('/list_products', (req, res) => {
-  res.send(JSON.stringify(listProducts));
+  const products = listProducts.map(product => ({
+    itemId: product.id,
+    itemName: product.name,
+    price: product.price,
+    initialAvailableQuantity: product.stock
+  }));
+  res.send(JSON.stringify(products));
 });
 
-app.get('/list_products/itemId(\\d+)', (req, res) => {
+app.get('/list_products/:itemId', async (req, res) => {
   const stockId = req.params.itemId
-  res.send(JSON.stringify(stockId))
+  const product = getItemById(stockId);
+  if (!product) {
+    res.json({status: "Product not found"})
+  }
+  const currentQuanity = await getCurrentReservedStockById(itemId) ?? product.stock;
+  res.json({
+    itemId: product.id,
+    itemName: product.name,
+    price: product.price,
+    initialAvailableQuantity: product.stock,
+    currentQuanity: currentQuanity
+  })
 })
 
 app.listen(port, () => {
@@ -66,11 +77,11 @@ client.on('error', (err) => {
   console.log('Redis client not connected to the server: ' + err);
 });
 
-function reserveStockById(itemId, stock) {
-  client.set(`item:${itemId}`, stock);
+async function reserveStockById(itemId, stock) {
+  await setAsync(`item:${itemId}`, stock);
 }
 
 async function getCurrentReservedStockById(itemId) {
   const reservedStock = await getAsync(`item:${itemId}`);
-  return parseInt(reservedStock) || 0;
+  return parseInt(reservedStock) || null;
 }

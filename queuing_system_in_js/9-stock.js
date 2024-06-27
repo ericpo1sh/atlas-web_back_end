a40956c399e3e9db7034ba031186e34ed5ca2abb
ericpo1sh <ericpo1sh@gmail.com>
2024-06-26 22:07:54 -0500

    finaly done

--- queuing_system_in_js/9-stock.js
+++ queuing_system_in_js/9-stock.js
@@ -32,6 +32,15 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
+async function reserveStockById(itemId, stock) {
+  await setAsync(`item:${itemId}`, stock);
+}
+
+async function getCurrentReservedStockById(itemId) {
+  const stock = await getAsync(`item.${itemId}`);
+  return stock ? parseInt(stock, 10) : null;
+}
+
 app.get('/list_products', (req, res) => {
   const products = listProducts.map(product => ({
     itemId: product.id,
@@ -48,7 +57,7 @@ app.get('/list_products/:itemId', async (req, res) => {
   if (!product) {
     res.json({status: "Product not found"})
   }
-  const currentQuanity = await getCurrentReservedStockById(itemId) ?? product.stock;
+  const currentQuanity = await getCurrentReservedStockById(stockId) ?? product.stock;
   res.json({
     itemId: product.id,
     itemName: product.name,
@@ -57,6 +66,22 @@ app.get('/list_products/:itemId', async (req, res) => {
     currentQuanity: currentQuanity
   })
 })
+
+app.get('/reserve_product/:itemId', async (req, res) => {
+  const stockId = req.params.itemId
+  const product = getItemById(stockId);
+  if (!product) {
+      res.json({ status: 'Product not found' });
+      return;
+  }
+  const currentStock = await getCurrentReservedStockById(stockId) ?? product.stock;
+  if (currentStock <= 0) {
+      res.json({ status: 'Not enough stock available', itemId });
+      return;
+  }
+  await reserveStockById(stockId, currentStock - 1);
+  res.json({ status: 'Reservation confirmed', stockId });
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
@@ -77,11 +102,3 @@ client.on('error', (err) => {
   console.log('Redis client not connected to the server: ' + err);
 });
 
-async function reserveStockById(itemId, stock) {
-  await setAsync(`item:${itemId}`, stock);
-}
-
-async function getCurrentReservedStockById(itemId) {
-  const reservedStock = await getAsync(`item:${itemId}`);
-  return parseInt(reservedStock) || null;
-}
